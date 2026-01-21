4d60e5918dc452e6b7314c9e7ee053bbf6bb5501
saces <saces@c-base.org>
2026-01-21 06:51:58 +0100

    fix typo

--- smal/src/pymxutils/mxutils/accountinfo.py
+++ smal/src/pymxutils/mxutils/accountinfo.py
@@ -8,7 +8,7 @@ def accountinfo():
         return 1
 
     url = sys.argv[1].encode(encoding="utf-8")
-    tk = sys.argv[1].encode(encoding="utf-8")
+    tk = sys.argv[2].encode(encoding="utf-8")
 
     r = lib.cli_accountinfo(url, tk)
     result = ffi.string(r)
